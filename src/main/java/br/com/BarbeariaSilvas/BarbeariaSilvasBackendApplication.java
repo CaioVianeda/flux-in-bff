c47f456925d1d3928557f322e412dae2e848582e
CaioVianeda <caio.vianwda@gmail.com>
2024-04-06 13:45:37 -0300

    Inicializado o projeto

---
+++ src/main/java/br/com/BarbeariaSilvas/BarbeariaSilvasBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.BarbeariaSilvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaSilvasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaSilvasBackendApplication.class, args);
+	}
+
+}
